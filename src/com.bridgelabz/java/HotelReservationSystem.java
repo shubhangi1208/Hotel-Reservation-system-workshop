26754ff6e20e4c6b9c23be15374afb9b1b550904
shubhangi1208 <ashubhangi1996@gmail.com>
2022-02-07 21:33:38 +0530

    ADDED:Ability to Add hotels

--- src/com.bridgelabz/java/HotelReservationSystem.java
+++ src/com.bridgelabz/java/HotelReservationSystem.java
@@ -6,24 +6,38 @@ public class HotelReservationSystem {
     public static void main(String args[]) {
         System.out.println("Welcome to Hotel Reservation System"); //Welcome message
         addHotel();
+        showhotel();
     }
-ArrayList<Hotel> list= new ArrayList<Hotel>();
+    static ArrayList<Hotel>  list= new ArrayList<Hotel>();
     public static void addHotel(){
+        Hotel hotel =new Hotel();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Hotel Name :");
-        String hotelName = sc.next();
+        System.out.println("Enter the First Name :");
+        hotel.setHotelName(sc.next());
         System.out.println("Enter the weekdays Rates :");
-        int weekdaysRates = sc.nextInt();
+        hotel.setWeekdaysRates(sc.nextInt());
         System.out.println("Enter the weekends Rates :");
-        String weekendsRates = sc.next();
+        hotel.setWeekendsRates(sc.nextInt());
+        System.out.println("Enter the weekdays for rewards customer Rates :");
+       hotel.setWeekdaysRatesforRewards();
         System.out.println("Enter the weekends for rewards customer Rates :");
-        String weekdaysRatesforRewards = sc.next();
-        System.out.println("Enter the weekends Rates :");
-        String weekendsRatesforRewards = sc.next();
+        hotel.setWeekendsRatesforRewards();
         System.out.println("Enter the Start Date :");
-        String startDate = sc.next();
+        hotel.setStartDate();
         System.out.println("Enter the Ending Date :");
-        String endDate = sc.next();
+        hotel.setEndDate();
+    }
+    public static void showhotel(){
+        for (Hotel hotel : list){
+            System.out.println("hotel Details -");
+            System.out.println("Hotel Name "+ hotel.hotelName);
+            System.out.println("Weekdays Rates :"+ hotel.weekdaysRates);
+            System.out.println("Weekends Rates :"+hotel.weekdaysRates);
+            System.out.println("Weekdays for rewards customer Rates : "+ hotel.weekdaysRatesforRewards);
+            System.out.println("weekends for rewards customer Rates :" + hotel.weekendsRatesforRewards);
+            System.out.println("Start Date :"+ hotel.startDate);
+            System.out.println("Ending Date"+ hotel.endDate);
 
+        }
     }
 }
